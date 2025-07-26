d63dbf21b8968ff69cf5d3969988d281c5bb6793
Yerko Barriga <yerkobarriga14@gmail.com>
2025-07-26 00:35:05 -0400

    love123

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/cartaAmor/', // Asegúrate de que la base sea correcta para tu despliegue
 })
